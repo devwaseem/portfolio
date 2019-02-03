55f0b8663a86c8169aa132015c4a31fc6f9d1f4a
Waseem <waseem07799@gmail.com>
2019-02-04 01:24:00 +0530

    Fixed module not found error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.styles.css'
 import App from './App';
-import "animate.css/animate.min.css";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
